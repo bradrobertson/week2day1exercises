b7d38dd710eb75bb5ae6482c6ea1702d5d51670b
Brad Robertson <bradleyrobertson@gmail.com>
2014-03-10 20:35:13 -0400

    p tags are blue

--- app.rb
+++ app.rb
@@ -3,10 +3,15 @@ require 'sinatra/reloader'
 
 get '/' do
   "
+  <style>
+    p{ color: blue;}
+  </style>
   <h1 id='someid'>SOme Heading</h1>
   <p>Hello World!</p>
   <ol>
     <li>Walk the dog</li>
+    <li>Walk the dog again</li>
+    <li>Walk the dog one more time</li>
   </ol>
   "
 end
