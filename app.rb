16e4a7e242a5651cd091687929a03be852cd7e2a
Brad Robertson <bradleyrobertson@gmail.com>
2014-03-10 21:08:13 -0400

    added some views

--- app.rb
+++ app.rb
@@ -2,18 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  "
-  <style>
-    p{ color: blue;}
-    .done { text-decoration: line-through;}
-    li:hover{ background-color: grey;}
-  </style>
-  <h1 id='someid'>SOme Heading</h1>
-  <p>Hello World!</p>
-  <ol>
-    <li class='done'>Walk the dog</li>
-    <li>Walk the dog again</li>
-    <li>Walk the dog one more time</li>
-  </ol>
-  "
+  erb :index
+end
+
+get '/hello/:name' do
+  name = params[:name]
+
+  erb :hello, locals: {
+    name: name,
+    x: Time.now
+  }
 end
