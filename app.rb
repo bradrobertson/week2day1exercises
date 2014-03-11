cc8f361da40123a5291d4761d947791676801613
Brad Robertson <bradleyrobertson@gmail.com>
2014-03-10 20:38:01 -0400

    todos can be hovered

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ get '/' do
   <style>
     p{ color: blue;}
     .done { text-decoration: line-through;}
+    li:hover{ background-color: grey;}
   </style>
   <h1 id='someid'>SOme Heading</h1>
   <p>Hello World!</p>
